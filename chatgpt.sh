6ac28120b054c9d3bec9520e95ba226aa7bd6632
jsonijr@gmail.com <netosj@yahoo.com>
2024-01-30 12:00:15 -0300

    Reset colour on exit.

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.36.1  jan/2024  by mountaineerbr  GPL+3
+# v0.36.2  jan/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -3055,7 +3055,7 @@ function cleanupf
        	do 	kill -- $pid 2>/dev/null;
        	done;
 	wait ${PIDS[@]}  &>/dev/null;
-       	echo $'[exit]' >&2;
+       	printf "${NC}%s\n" '[exit]' >&2;
 }
 
 
