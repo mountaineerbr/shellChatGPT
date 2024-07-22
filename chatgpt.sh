cab20f4575f6c3d09903c4c29d6e59b0e63f99fc
jsonijr@gmail.com <netosj@yahoo.com>
2024-07-22 11:16:13 -0300

    Turn off folding when chat command "//md [software]" is set, for example, mdless, bat with options, and less...
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.65.2  jul/2024  by mountaineerbr  GPL+3
+# v0.65.3  jul/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -1444,6 +1444,8 @@ function set_mdcmdf
 			eval "function mdf { 	$* \"\$@\" ;}";
 			;;
 	esac;
+	#turn off folding for some software
+	case "$1" in mdless*|less*|bat?*) OPTFOLD=1; cmd_runf /fold;; esac;
 }
 
 #set a terminal web browser
