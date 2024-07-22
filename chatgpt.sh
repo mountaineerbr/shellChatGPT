fa8362e2f50e66cbf6430e7a98dd886d5ba9c4de
jsonijr@gmail.com <netosj@yahoo.com>
2024-07-22 19:48:11 -0300

    Minor, fix implementationof -P and -PP in chat environment.
    These were inverted in the case statement logic!.
    
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.66.2  jul/2024  by mountaineerbr  GPL+3
+# v0.66.3  jul/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -1573,7 +1573,7 @@ function cmd_runf
 			fi
 			((OPTCLIP)) && ${CLIP_CMD:-false} <<<"$var" && echo 'Clipboard set!' >&2
 			;;
-		-P|P) 	cmd_runf -HHH;; -PP|PP) 	cmd_runf -HH;;
+		-P|P) 	cmd_runf -HH;; -PP|PP) 	cmd_runf -HHH;;
 		j|jump)
 			__cmdmsgf 'Jump:' 'append response primer'
 			JUMP=1 REPLY=
