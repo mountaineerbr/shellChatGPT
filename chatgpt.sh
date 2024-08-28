364ed47c15f3cf783b3a2a6c41b99cd3050cf3e0
jsonijr@gmail.com <netosj@yahoo.com>
2024-08-28 18:05:22 -0300

    Update is_mdf() to be more strict when checking for markdown in response.
    As this function is used to auto-detect markdown in response and enable markdown rendering, it turns out that some answers only have ordered or unordered lists, and if that is the *only* markdown, it is not worth it turning on markdown automatically. However, is the lists are idented, then markdown will be turned on.
    
    Maybe in the future we will remove the markdown lists regex, because really just turn on markdown rendering when tehre is enough markdown to render!
    
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.74.1  aug/2024  by mountaineerbr  GPL+3
+# v0.74.2  aug/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -2934,7 +2934,13 @@ function is_visionf
 
 function is_mdf
 {
-	[[ "\\n$1" =~ (\*\*|__|\[[^\]]*\]\([^\)]*\)|\\n\ *\`\`\`|\\n\#\#*\ |\\n\ *[\*-]\ |\\n\ *[0-9][0-9]*\.\ ) ]]
+	[[ "\\n$1" =~ (\
+[*_][*_][[:alnum:]]|\
+\\n\ *\`\`\`|\
+\\n\#\#*\ |\
+\\n\ \ *[*-]\ |\
+\\n\ \ *[0-9][0-9]*\.\ |\
+\[[^\]]*\]\([^\)]*\)) ]]
 }
 
 function _is_docf
