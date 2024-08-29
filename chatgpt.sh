f76cf193fa023e14a91bdbbec4d6a972af5fd955
jsonijr@gmail.com <netosj@yahoo.com>
2024-08-28 22:26:45 -0300

    Comment out executing trim_silencef() function to remove silence from voice record.
    The threshold is probably too low to actually remove any significant amount of "silence" from the voice record.
    
    This works well with professionally recorded audio to remove silence periods, but voice record in random environments usually have a lot of background noise.
    
    So I am commenting this function out as it is probably an overhead at this time. I want to test it more to see if it actually makes any significant "silence" removal.
    
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.74.2  aug/2024  by mountaineerbr  GPL+3
+# v0.74.3  aug/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -3147,10 +3147,10 @@ function recordf
 
 	rec_killf $pid $termux;
        	trap 'exit' INT;
-	wait $pid &&
-	if command -v ffmpeg >/dev/null 2>&1
-	then 	trim_silencef "$FILEINW";
-	fi
+	wait $pid 
+	#if command -v ffmpeg >/dev/null 2>&1
+	#then 	trim_silencef "$FILEINW";
+	#fi
 	return ${ret:-0};
 }
 #avfoundation for macos: <https://apple.stackexchange.com/questions/326388/>
