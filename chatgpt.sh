abd291fe57989662918f7d2ba13922f41ad0ef06
jsonijr@gmail.com <netosj@yahoo.com>
2024-10-12 13:09:39 -0300

    2x Run set_optsf() in a forked shell in command /info.
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.78.7  oct/2024  by mountaineerbr  GPL+3
+# v0.78.8  oct/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -2117,7 +2117,7 @@ function cmd_runf
 			fi
 			;;
 		i|info)
-			(  #typeset hurl hurlv modmodal rseq sseq stop
+			(  unset hurl hurlv modmodal rseq sseq stop
 			((OLLAMA)) && hurl='ollama-url' hurlv=${OLLAMA_API_HOST}${ENDPOINTS[EPN]};
 			((GOOGLEAI)) && hurl='google-url' hurlv=${GOOGLE_API_HOST}${ENDPOINTS[EPN]};
 			((ANTHROPICAI)) && hurl='anthropic-url' hurlv=${ANTHROPIC_API_HOST}${ENDPOINTS[EPN]};
