0073bc5478ff293b19410acb6b61d37ca86c819b
jsonijr@gmail.com <netosj@yahoo.com>
2024-10-13 16:37:01 -0300

    Bump minor version number form last code update.
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.78.11  oct/2024  by mountaineerbr  GPL+3
+# v0.78.12  oct/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
