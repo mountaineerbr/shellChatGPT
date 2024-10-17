f86ed23c1084634f095468fa3d27216a7f790842
jsonijr@gmail.com <netosj@yahoo.com>
2024-10-17 16:22:40 -0300

    Minor improve text in text editor interface bad edit warning prompt.
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/Whisper/TTS
-# v0.79.4  oct/2024  by mountaineerbr  GPL+3
+# v0.79.5  oct/2024  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -2548,7 +2548,7 @@ function edf
 		    }
 		fi
 		[[ "$pos" != "${pre}"* ]] || [[ "$pos" = *"${rest:-%#%#}" ]]
-	do 	_warmsgf "Warning:" "Bad edit: [E]dit, [c]ontinue, [/]cmd, [r]edo or [a]bort? " ''
+	do 	_warmsgf "Bad edit:" "[E]dit, [c]ontinue, [/]cmd, [r]edo or [a]bort? " ''
 		reply=$(read_charf)
 		case "$reply" in
 			[-/!]) read_mainf -i "$reply" reply;
