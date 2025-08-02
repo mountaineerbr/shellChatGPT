110d04a9da303a170e7a38c496d3db50b19e4adf
jsonijr@gmail.com <netosj@yahoo.com>
2025-08-02 17:48:37 -0300

    +Minor: change one more sanity newline code for smoother user interface.
    ---

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # chatgpt.sh -- Shell Wrapper for ChatGPT/DALL-E/STT/TTS
-# v0.108.8  aug/2025  by mountaineerbr  GPL+3
+# v0.108.9  aug/2025  by mountaineerbr  GPL+3
 set -o pipefail; shopt -s extglob checkwinsize cmdhist lithist histappend;
 export COLUMNS LINES; ((COLUMNS>2)) || COLUMNS=80; ((LINES>2)) || LINES=24;
 
@@ -3462,7 +3462,7 @@ function cmdf
 				[[ ${argv[0]:0:32} = [/!-][A-Za-z][A-Za-z]*([A-Za-z]) ]]
 			then
 				for ((n=1;n<${#argv[0]};n++))
-				do 	((n==1)) || echo >&2;
+				do 	((n<2)) || echo >&2;
 					cmdmsgf "Command Run:" "${argv[0]:0:1}${argv[0]:n:1}";
 
 					cmdf ${argv[0]:0:1}${argv[0]:n:1} || {
